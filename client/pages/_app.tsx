f58dbcf96e03e6bb3318da3599d1b8a3c9abdd8b
jp <plutakuba@gmail.com>
2024-02-15 20:54:24 +0100

    feat: added login module

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,13 @@
+import '../styles/globals.css'
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+        <div className='flex flex-col md:flex-row h-full min-h-screen font-sans'>
+            <Component {...pageProps} />
+        </div>
+
+    </>
+    )
 }
